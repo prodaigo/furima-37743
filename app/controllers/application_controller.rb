ad7aaccc3e97885a22586ec39f75e15468650441
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-12 18:09:22 +0900

    Rubocop実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth  #メソッド呼び出し
+  before_action :basic_auth  # メソッド呼び出し
 
-  private  #クラス外から呼び出すことができないメソッドを定義
+  private # クラス外から呼び出すことができないメソッドを定義
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|  #Basic認証を実装するためのメソッド
-      username == 'daigo' && password == '37743'  #任意のusernameとpassword指定
+    authenticate_or_request_with_http_basic do |username, password| # Basic認証を実装するためのメソッド
+      username == 'daigo' && password == '37743'  # 任意のusernameとpassword指定
     end
   end
-
-
 end
