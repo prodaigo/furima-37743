84222c9da4328e485d8b441fb1f29425fc2bf97e
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-14 14:20:13 +0900

    会員登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth  # メソッド呼び出し
+  before_action :configure_permitted_parameters, if: :devise_controller?  #deviseに関するコントローラーの処理であれば、メソッド呼び出し
 
   private # クラス外から呼び出すことができないメソッドを定義
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password| # Basic認証を実装するためのメソッド
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 任意のusernameとpassword指定
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])  # deviseのUserモデルにパラメーターを許可
   end
 end
