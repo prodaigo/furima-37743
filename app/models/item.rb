29db38547b791c96ca99db4b862867416b22e74f
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-15 15:42:29 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  # has_one :purchase
+  # has_many :comments
+  # has_one_attached :image
+  
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipment_date_id
+    # validates :image
+  end
 end
