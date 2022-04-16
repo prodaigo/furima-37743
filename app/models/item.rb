1180a05d4a9d7e8e82531dc6157d7a8bc9314708
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-16 20:06:22 +0900

    バリデーションとアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,29 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :delivery_charge
+    belongs_to :prefecture
+    belongs_to :shipment_date
+
   belongs_to :user
   # has_one :purchase
   # has_many :comments
-  # has_one_attached :image
+  has_one_attached :image
   
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
-    validates :category_id
-    validates :condition_id
-    validates :delivery_charge_id
-    validates :prefecture_id
-    validates :shipment_date_id
-    # validates :image
+    validates :image
+    with_options numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ } do
+      validates :price
+    end
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_charge_id
+      validates :prefecture_id
+      validates :shipment_date_id
+    end
   end
 end
