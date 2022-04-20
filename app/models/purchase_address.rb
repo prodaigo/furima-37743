6b4ec86856079adf970abb450040aec52d467777
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-20 17:20:10 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,17 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id
+
+  with_options presence: true do
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number' }
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
