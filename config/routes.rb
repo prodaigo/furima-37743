f3b929e6432d52aae0782962c80e20d9dca24483
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-20 17:20:58 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users  #ユーザー管理機能に必要なルーティング自動生成
   root to: 'items#index'  #ルートパス指定
-  resources :items  #itemのアクションのルーティング自動生成
+  resources :items do  #itemのアクションのルーティング自動生成
+    resources :purchases, only: [:index, :create]
+  end
 end
