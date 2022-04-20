e6a22b9588591bd33401a64e03152267f341c56e
prodaigo <soccer.0125.dtp@gmail.com>
2022-04-20 17:23:44 +0900

    複数テーブルに情報を保存できる機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,34 @@
 class PurchasesController < ApplicationController
+  before_action :find_params
+  before_action :authenticate_user!
+  before_action :move_to_index
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def find_params
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
+  end
 end
